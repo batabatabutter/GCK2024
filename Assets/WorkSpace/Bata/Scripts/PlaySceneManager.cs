76c863fb1c3ed03179333080beb898a22232f277
batabatabutter <116615347+batabatabutter@users.noreply.github.com>
2024-03-13 12:33:01 +0900

    プレイシーンマネージャーにコアを格納できるように

--- Assets/WorkSpace/Bata/Scripts/PlaySceneManager.cs
+++ Assets/WorkSpace/Bata/Scripts/PlaySceneManager.cs
@@ -6,6 +6,8 @@ public class PlaySceneManager : MonoBehaviour
 {
     //  プレイヤー
     private GameObject m_player;
+    //  コア
+    private List<GameObject> m_cores;
 
     // Start is called before the first frame update
     void Start()
@@ -23,4 +25,9 @@ public class PlaySceneManager : MonoBehaviour
     public void SetPlayer(GameObject player) { m_player = player; }
     //  プレイヤーの取得
     public GameObject GetPlayer() { return m_player; }
+
+    //  コアの設定
+    public void AddCore(GameObject core) { m_cores.Add(core); }
+    //  コア達の取得
+    public List<GameObject> GetCores() { return m_cores; }
 }
