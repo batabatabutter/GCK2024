5e27174590917a9bc39916cd82299f055e5bc32c
batabatabutter <116615347+batabatabutter@users.noreply.github.com>
2024-03-28 12:09:40 +0900

    イラスト追加＋HPUIアーマー対応

--- Assets/WorkSpace/Bata/UI/HP/HPUI.cs
+++ Assets/WorkSpace/Bata/UI/HP/HPUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
+using UnityEngine.UI;
 using static UnityEditor.PlayerSettings;
 
 public class HPUI : MonoBehaviour
@@ -11,6 +12,9 @@ public class HPUI : MonoBehaviour
     [SerializeField] private GameObject m_hpGauge;
     [SerializeField] private GameObject m_hpGaugeFrame;
     [SerializeField] private Vector2 m_hpOffset;
+    [SerializeField] private UnityEngine.Color m_hpColor;
+    [SerializeField] private UnityEngine.Color m_armorColor;
+    [SerializeField] private UnityEngine.Color m_emptyColor;
 
     //  シーンマネージャー
     private PlaySceneManager m_playSceneManager;
@@ -61,16 +65,18 @@ public class HPUI : MonoBehaviour
         //  HP状態を参照しゲージ変化
         int hpVal = 0;
         int maxHpVal = 0;
+        int armorVal = 0;
         hpVal = m_player.GetComponent<Player>().HitPoint;
         maxHpVal = m_player.GetComponent<Player>().MaxLife;
-
+        armorVal = m_player.GetComponent<Player>().Armor;
+        
         //  UI変化
         //  生成位置
         Vector2 size = m_hpGaugeFrame.GetComponent<RectTransform>().sizeDelta;
         Vector3 pos;
-        for (int i = 0; i < maxHpVal; i++)
+        for (int i = 0; i < maxHpVal + armorVal; i++)
         {
-            //  最大HPよりUIが少なかったら生成
+            //  最大HPとアーマーよりUIが少なかったら生成
             if (i >= m_hpGaugeObject.Count)
             {
                 //  座標
@@ -82,10 +88,13 @@ public class HPUI : MonoBehaviour
             }
 
             //  HPがあれば色付き
-            if (i < hpVal) m_hpGaugeObject[i].SetActive(true);
-            else m_hpGaugeObject[i].SetActive(false);
+            if (i < hpVal) m_hpGaugeObject[i].GetComponent<RawImage>().color = m_hpColor;
+            else if(i >= maxHpVal && i < maxHpVal + armorVal) m_hpGaugeObject[i].GetComponent<RawImage>().color = m_armorColor;
+            else m_hpGaugeObject[i].GetComponent<RawImage>().color = m_emptyColor;
         }
-        
+
+        //  アーマー
+
         //  デバッグ状態
         if (m_debug)
         {
