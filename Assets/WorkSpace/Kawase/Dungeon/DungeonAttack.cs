61851e0bab2d3f098469af2b9ded0f220623ef2e
it222064 <it222064@trident.ac.jp>
2024-03-30 17:08:14 +0900

    コウゲキ後にクールタイム加算

--- Assets/WorkSpace/Kawase/Dungeon/DungeonAttack.cs
+++ Assets/WorkSpace/Kawase/Dungeon/DungeonAttack.cs
@@ -20,9 +20,13 @@ public class DungeonAttack : MonoBehaviour
     [SerializeField] bool isroll;
     [SerializeField] bool isbank;
 
-    [Header("攻撃間隔")]
-    [SerializeField] float attackCoolTime = 1.0f;
+    [Header("--------------------------------------------")]
 
+    [Header("攻撃間隔")]
+    [SerializeField] float attackCoolTime = 3.0f;
+
+    [Header("coreの攻撃間隔が２倍になる距離")]
+    [SerializeField] float twiceAttackLength = 20;
 
     [Header("--------------------------------------------")]
 
@@ -36,6 +40,9 @@ public class DungeonAttack : MonoBehaviour
     [SerializeField] GameObject highlight;
     [Header("落石の生成する高さ")]
     public float heightRock = 3.0f;
+    [Header("コウゲキ時間")]
+    [SerializeField] float fallTime = 4.0f;
+
     [Header("--------------------------------------------")]
     [Header("転がる岩")]
     [SerializeField] GameObject rollRock;
@@ -50,27 +57,30 @@ public class DungeonAttack : MonoBehaviour
     [SerializeField] float down;
     [Header("左から")]
     [SerializeField] float left;
+    [Header("コウゲキ時間")]
+    [SerializeField] float rollTime = 7.0f;
+
     [Header("--------------------------------------------")]
     [Header("土手")]
     [SerializeField] GameObject bank;
     [Header("土手ハイライト")]
     [SerializeField] GameObject bankHighlight;
+    [Header("コウゲキ時間")]
+    [SerializeField] float bankTime = 4.0f;
 
-    [Header("coreの攻撃間隔が２倍になる距離")]
-    [SerializeField] float twiceAttackLength = 20;
 
 
-    float keepCoolTime;
+    float m_keepCoolTime;
 
-    GameObject target;
-    GameObject core;
+    GameObject m_target;
+    GameObject m_core;
 
     // Start is called before the first frame update
     void Start()
     {
-        target = SceneManager.GetPlayer();
-        core = SceneManager.GetCore();
-        keepCoolTime = attackCoolTime;
+        m_target = SceneManager.GetPlayer();
+        m_core = SceneManager.GetCore();
+        m_keepCoolTime = attackCoolTime;
     }
 
     // Update is called once per frame
@@ -79,9 +89,9 @@ public class DungeonAttack : MonoBehaviour
         int ratio = 1;
 
         //coreとプレイヤーが近いとコウゲキが2倍
-        if(core != null) 
+        if(m_core != null) 
         {
-            if (Vector2.Distance(core.transform.position, target.transform.position) < twiceAttackLength)
+            if (Vector2.Distance(m_core.transform.position, m_target.transform.position) < twiceAttackLength)
             {
                 ratio = 2;
             }
@@ -98,9 +108,14 @@ public class DungeonAttack : MonoBehaviour
             bool checkAttacked = false;
             //無限ループよけ
             int outNum = 10;
+
+            //コウゲキに合わせた時間加算
+            float attackAddTime = 0.0f;
+
             //コウゲキしなかった場合再抽選
             while (!checkAttacked)
             {
+
                 //マジックナンバーで行かして頂きまs
                 int r = Random.Range(0, 3);
 
@@ -108,17 +123,24 @@ public class DungeonAttack : MonoBehaviour
                 {
                     FallrockAttack();
                     checkAttacked = true;
+
+                    attackAddTime = fallTime;
                 }
 
                 if (isroll && r == 1)
                 {
                     RockRollingAttack();
                     checkAttacked = true;
+
+                    attackAddTime = rollTime;
+
                 }
                 if (isbank && r == 2)
                 {
                     BankAttack();
                     checkAttacked = true;
+                    attackAddTime = bankTime;
+
                 }
 
                 outNum--;
@@ -127,14 +149,16 @@ public class DungeonAttack : MonoBehaviour
                     break;
             }
             //クールタイムリセット
-            attackCoolTime = keepCoolTime;
+            attackCoolTime = m_keepCoolTime + attackAddTime;
+
+            Debug.Log(attackAddTime);
         }
     }
 
     private void FallrockAttack()
     {
-        Vector3 rockfallPos = new Vector3(target.transform.position.x, target.transform.position.y + heightRock, 0);
-        Vector3 highlightPos = new Vector3(target.transform.position.x, target.transform.position.y - HEIGLIGHT_HEIGHT, 0);
+        Vector3 rockfallPos = new Vector3(m_target.transform.position.x, m_target.transform.position.y + heightRock, 0);
+        Vector3 highlightPos = new Vector3(m_target.transform.position.x, m_target.transform.position.y - HEIGLIGHT_HEIGHT, 0);
 
         Instantiate(rockfall, rockfallPos, Quaternion.identity);
         Instantiate(highlight, highlightPos, Quaternion.identity);
@@ -151,26 +175,26 @@ public class DungeonAttack : MonoBehaviour
 
         if(rand == (int)Direction.UP)
         {
-            rollingPos = new Vector3(target.transform.position.x,target.transform.position.y + up, 0);
+            rollingPos = new Vector3(m_target.transform.position.x,m_target.transform.position.y + up, 0);
 
             rollingRotation = 180;
 
         }
         else if(rand == (int)Direction.RIGHT)
         {
-            rollingPos = new Vector3(target.transform.position.x + right, target.transform.position.y, 0);
+            rollingPos = new Vector3(m_target.transform.position.x + right, m_target.transform.position.y, 0);
             rollingRotation = 90;
 
         }
         else if (rand == (int)Direction.DWON)
         {
-            rollingPos = new Vector3(target.transform.position.x, target.transform.position.y - down, 0);
+            rollingPos = new Vector3(m_target.transform.position.x, m_target.transform.position.y - down, 0);
             rollingRotation = 0;
 
         }
         else
         {
-            rollingPos = new Vector3(target.transform.position.x - left, target.transform.position.y, 0);
+            rollingPos = new Vector3(m_target.transform.position.x - left, m_target.transform.position.y, 0);
             rollingRotation = 270;
 
         }
@@ -182,7 +206,7 @@ public class DungeonAttack : MonoBehaviour
     }
     private void BankAttack()
     {
-        Vector3 pos = target.transform.position;
+        Vector3 pos = m_target.transform.position;
 
         pos.x -= 1;
         pos.y += 1;
