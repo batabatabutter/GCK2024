ff4b02daefe5381b2e014777058eef8531c007cc
mizuki <crystal23159@gmail.com>
2024-04-01 11:44:35 +0900

    未到達地点はマップに表示しないように追加

---
+++ Assets/WorkSpace/Mizuiki/DataBase/DungeonDataBase.cs
@@ -0,0 +1,11 @@
+
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DungeonDataBase", menuName = "CreateDungeonDataBase")]
+public class DungeonDataBase : ScriptableObject
+{
+	[Header("ƒ_ƒ“ƒWƒ‡ƒ“‚ÌCSV")]
+	public Object[] dungeonCSV;
+
+}
