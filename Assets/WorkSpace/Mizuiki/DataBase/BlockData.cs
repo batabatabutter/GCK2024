790d138a697d7da9361f73983f3b84334a18c721
mizuki <crystal23159@gmail.com>
2024-04-05 15:09:39 +0900

    ブロックデータの調整 プレイヤーの採掘仕様変更

--- Assets/WorkSpace/Mizuiki/DataBase/BlockData.cs
+++ Assets/WorkSpace/Mizuiki/DataBase/BlockData.cs
@@ -8,30 +8,33 @@ public class BlockData : ScriptableObject
 	[System.Serializable]
 	public enum BlockType
 	{
-		CORE,		// コア
-		BEDROCK,	// 岩盤
-		STONE,		// 石
-		COAL,       // 石炭
-		STEEL,		// 鉄
-		TIN,		// 錫
-		LEAD,		// 鉛
+		STONE = 0,		// 石
+		COAL,			// 石炭
+		STEEL,			// 鉄
+		TIN,			// 錫
+		LEAD,			// 鉛
 
-		TOACH,		// 松明
-		BOMB,		// 爆弾
+		SPECIAL = 100,	// ここから特殊ブロック
+		CORE,			// コア
+		BEDROCK,		// 岩盤
+		DENGEROUS,		// 危険物
 
 		BIRTHDAY_BLOCK = 200,	// ここから誕生石シリーズ
-		BIR_GARNET,			// ガーネット
-		BIR_AMETHYST,       // アメジスト
-		BIR_AQUAMARINE,     // アクアマリン
-		BIR_DIAMOND,        // ダイヤモンド
-		BIR_EMERALD,        // エメラルド
-		BIR_PEARL,          // パール
-		BIR_RUBY,           // ルビー
-		BIR_PERIDOT,        // ペリドット
-		BIR_SAPPHIRE,       // サファイア
-		BIR_OPAL,           // オパール
-		BIR_TOPAZ,          // トパーズ
-		BIR_TURQUOISE,      // ターコイズ
+		BIR_GARNET,				// ガーネット
+		BIR_AMETHYST,			// アメジスト
+		BIR_AQUAMARINE,			// アクアマリン
+		BIR_DIAMOND,			// ダイヤモンド
+		BIR_EMERALD,			// エメラルド
+		BIR_PEARL,				// パール
+		BIR_RUBY,				// ルビー
+		BIR_PERIDOT,			// ペリドット
+		BIR_SAPPHIRE,			// サファイア
+		BIR_OPAL,				// オパール
+		BIR_TOPAZ,				// トパーズ
+		BIR_TURQUOISE,			// ターコイズ
+
+		TOACH = 1000,	// 松明
+		BOMB,			// 爆弾
 
 		OVER
 	}
