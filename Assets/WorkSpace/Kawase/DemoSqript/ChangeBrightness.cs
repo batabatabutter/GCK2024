e7316781eb6d7db01d1a42275986d798c7b630d6
batabatabutter <116615347+batabatabutter@users.noreply.github.com>
2024-04-26 11:39:11 +0900

    極力軽量処理

--- Assets/WorkSpace/Kawase/DemoSqript/ChangeBrightness.cs
+++ Assets/WorkSpace/Kawase/DemoSqript/ChangeBrightness.cs
@@ -5,26 +5,31 @@ using System.Linq;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
+using static Unity.Collections.AllocatorManager;
 
 public class ChangeBrightness : MonoBehaviour
 {
 	//明るさの最大値
 	private static readonly int MAX_BRIGHTNESS = 7;
 	public int GetMAXBRIGHTBESS() { return MAX_BRIGHTNESS; }
+    //  距離光源
+    private static readonly float DISTANCE_LIGHT = 17.0f;
 
+    //LightList
+    public List<ObjectLight> m_lightList = new();
+    //  現在の受け取りライトレベル
+    private int m_nowLightLevel = 0;
 
-	//LightList
-	public List<ObjectLight> m_lightList = new();
-	public List<Vector3> m_lightPositionList = new();
+    //  プレイヤーの座標
+    private Transform m_playerTr;
 
-	//  プレイヤーの座標
-	private Transform m_playerTr;
-	//  光源当たり判定
-	//private Collider[] m_colliders;
-	//  衝突判定起動中か
-	//private bool m_colldiersFlag = true;
-	//  距離光源
-	private const float DISTANCE_LIGHT = 17.0f;
+    //  光源
+    private ObjectLight m_objlight;
+    //  光源衝突判定
+    private Collider[] m_lightColliders;
+    //  光源衝突判定フラグ
+    private bool m_lightColidersFlag = true;
+
 	[Header("ブロック")]
 	[SerializeField] private /*Block*/ObjectAffectLight m_affectLight;
 
@@ -33,8 +38,17 @@ public class ChangeBrightness : MonoBehaviour
 	{
 		//黒くする
 		ChangeBlack();
-		// ライトの影響を受けるオブジェクトの取得
-		if (gameObject.TryGetComponent(out ObjectAffectLight affectLight))
+
+        //  光源取得
+        if (gameObject.TryGetComponent(out ObjectLight objLight))
+        {
+            m_objlight = objLight;
+            m_nowLightLevel = m_objlight.LightLevel;
+            m_lightColliders = GetComponents<Collider>();
+        }
+
+        // ライトの影響を受けるオブジェクトの取得
+        if (gameObject.TryGetComponent(out ObjectAffectLight affectLight))
 		{
 			m_affectLight = affectLight;
 		}
@@ -43,84 +57,112 @@ public class ChangeBrightness : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		//  プレイヤーとの距離が一定以上離れていたら処理しない
-		if (m_playerTr == null)
-		{
-			if (Time.frameCount % 60 == 0) // 60FPS固定なら、
-			{
-				Debug.Log("Error:BlockにPlyer座標が入ってない：" + this);
-			}
-			return;
-		}
-		if (Vector2.Distance(transform.position, m_playerTr.position) > DISTANCE_LIGHT) return;
+        //  光の強さがMAXなら処理しない
+        if (m_objlight)
+        {
+            if (m_objlight.LightLevel >= MAX_BRIGHTNESS)
+            {
+                m_affectLight.ReceiveLightLevel = MAX_BRIGHTNESS;
+                return;
+            }
+        }
 
-		//if (Vector2.Distance(transform.position, m_playerTr.position) > DISTANCE_LIGHT)
-		//{
-		//    //  判定がついているなら
-		//    if (m_colldiersFlag)
-		//    {
-		//        //  判定を消滅
-		//        m_colldiersFlag = false;
-		//        foreach (Collider collider in m_colliders)
-		//        {
-		//            collider.enabled = false;
-		//        }
-		//    }
-		//    return;
-		//}
-		//else
-		//{
-		//    //  判定がついていないなら
-		//    if (!m_colldiersFlag)
-		//    {
-		//        //  判定つける
-		//        m_colldiersFlag = true;
-		//        foreach (Collider collider in m_colliders)
-		//        {
-		//            collider.enabled = true;
-		//        }
-		//    }
-		//}
+        //  光源レベル
+        int lv = 0;
+        if (m_objlight) lv = m_objlight.LightLevel;
 
-		//ライトリストの管理
-		for (int i = 0; i < m_lightList.Count; i++)
-		{
-			//無くなったら消す
-			if (m_lightList[i] == null)
-			{
-				RemoveLightList(i);
-			}
-			//離れたら消す(3は適当に大きめにしたdeleteLength的な)
-			else if (Mathf.Abs(Vector3.Distance(MyFunction.RoundHalfUp(m_lightList[i].gameObject.transform.position), gameObject.transform.position)) > m_lightList[i].LightLevel + 3)
-			{
-				RemoveLightList(i);
-			}
-		}
+        //  プレイヤーとの距離が一定以上離れていたら処理しない
+        if (m_playerTr == null)
+        {
+            if (Time.frameCount % 60 == 0) // 60FPS固定なら、
+            {
+                Debug.Log("Error:BlockにPlyer座標が入ってない：" + this);
+            }
+        }
+        else if (Vector2.Distance(transform.position, m_playerTr.position) > DISTANCE_LIGHT + lv)
+        {
+            //  光源がある場合の追加処理
+            if (m_objlight && m_lightColidersFlag)
+            {
+                //  衝突判定を消す
+                foreach (Collider col in m_lightColliders) { col.gameObject.SetActive(false); }
+                m_lightColidersFlag = false;
+            }
+            return;
+        }
+        else
+        {
+            //  光源がある場合の処理
+            if (m_objlight && !m_lightColidersFlag)
+            {
+                //  衝突判定をつける
+                foreach (Collider col in m_lightColliders) { col.gameObject.SetActive(true); }
+                m_lightColidersFlag = true;
+            }
+        }
 
-		//色の変更
-		ChangeColor();
+        ChangeColor();
+    }
 
-
-	}
-
-	void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
 	{
-		// 当たったものがライトではない
-		if (collision.gameObject.layer != LayerMask.NameToLayer("Light"))
+        //  光が最大以上なら処理しない
+        if (m_objlight)
+            if (m_objlight.LightLevel >= MAX_BRIGHTNESS) return;
+
+        // 当たったものがライトではない
+        if (collision.gameObject.layer != LayerMask.NameToLayer("Light"))
 			return;
 
-		// ライトのレベルが 0 以下
-		if (collision.gameObject.GetComponent<ObjectLight>().LightLevel <= 0)
+        //  ライトコンポ
+        var li = collision.gameObject.GetComponent<ObjectLight>();
+
+        // ライトのレベルが 0 以下
+        if (li.LightLevel <= 0)
 			return;
 
-		// 光源の追加
-		if (!CheckForObjectInList(collision.gameObject))
+        //  ライトのレベルが自分の光源レベル以下
+        if (m_objlight)
+            if (li.LightLevel <= m_objlight.LightLevel) return;
+
+        // 光源の追加
+        if (!CheckForObjectInList(collision.gameObject))
 		{
-			AddLightList(collision.gameObject);
-		}
-	}
+            m_lightList.Add(li);
+        }
+    }
 
-	private void ChangeColor()
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //  光が最大以上なら処理しない
+        if (m_objlight)
+            if (m_objlight.LightLevel >= MAX_BRIGHTNESS) return;
+
+        // 当たったものがライトではない
+        if (collision.gameObject.layer != LayerMask.NameToLayer("Light"))
+            return;
+
+        // ライトのレベルが 0 以下
+        if (collision.gameObject.GetComponent<ObjectLight>().LightLevel <= 0)
+            return;
+
+        //ライトリストの管理
+        for (int i = 0; i < m_lightList.Count; i++)
+        {
+            //無くなったら消す
+            if (m_lightList[i] == null)
+            {
+                RemoveLightList(i);
+            }
+            //離れたら消す(3は適当に大きめにしたdeleteLength的な)
+            else if (Vector3.Distance(MyFunction.RoundHalfUp(m_lightList[i].gameObject.transform.position), gameObject.transform.position) > m_lightList[i].LightLevel + 3)
+            {
+                RemoveLightList(i);
+            }
+        }
+    }
+
+    private void ChangeColor()
 	{
 		//例外処理
 		if ((m_lightList.Count == 0 || !m_lightList.Any()/* || m_lightList[0] == null || gameObject == null*/))
@@ -130,24 +172,36 @@ public class ChangeBrightness : MonoBehaviour
 		}
 		else
 		{
-			List<float> lightListV = new();
+            //  総合光レベル
+            int receiveLightLv = 0;
+            if (m_objlight) receiveLightLv = m_objlight.LightLevel;
 
-			for (int i = 0; i < m_lightList.Count; i++)
+            for (int i = 0; i < m_lightList.Count; i++)
 			{
-				if (m_lightList[i] == null)
-					continue;
+                //  明るさ最大なら処理終了
+                if (receiveLightLv >= MAX_BRIGHTNESS) break;
 
-				float lightLength = Mathf.Ceil(Vector3.Distance(MyFunction.RoundHalfUp(m_lightList[i].transform.position), this.transform.position));
+                //  nullならスキップ
+                if (m_lightList[i] == null)
+                    continue;
 
-				lightListV.Add(m_lightList[i].LightLevel - lightLength);
+                //  受けたレベルが自分以下ならスキップ
+                if (receiveLightLv >= m_lightList[i].LightLevel) continue;
 
+                //  光源距離計算
+                float lightLength = Mathf.Ceil(Vector3.Distance(MyFunction.RoundHalfUp(m_lightList[i].transform.position), this.transform.position));
 
-			}
+                receiveLightLv = math.max(receiveLightLv, m_lightList[i].LightLevel - (int)lightLength);
+            }
 
-			if (m_affectLight && !m_affectLight.IsDestroyed() && lightListV.Count() != 0)
-			{
-				m_affectLight.ReceiveLightLevel = Math.Max((int)lightListV.Max(), 0);
-			}
+            //  光源レベル設定
+            m_nowLightLevel = receiveLightLv;
+
+            //  ブロックが存在するなら
+            if (m_affectLight && !m_affectLight.IsDestroyed())
+            {
+                m_affectLight.ReceiveLightLevel = m_nowLightLevel;
+            }
 		}
 	}
 
@@ -160,8 +214,17 @@ public class ChangeBrightness : MonoBehaviour
 		}
 	}
 
-	// 引数のオブジェクトが既にある(Lightプレハブ)
-	bool CheckForObjectInList(GameObject obj)
+    //  明るくする
+    private void ChangeBright()
+    {
+        if (m_affectLight)
+        {
+            m_affectLight.ReceiveLightLevel = MAX_BRIGHTNESS;
+        }
+    }
+
+    // 引数のオブジェクトが既にある(Lightプレハブ)
+    bool CheckForObjectInList(GameObject obj)
 	{
 		// リスト内の各オブジェクトをチェック
 		foreach (ObjectLight item in m_lightList)
@@ -183,12 +246,10 @@ public class ChangeBrightness : MonoBehaviour
 	private void AddLightList(GameObject lightObj)
 	{
 		m_lightList.Add(lightObj.GetComponent<ObjectLight>());
-		m_lightPositionList.Add(lightObj.transform.position);
 	}
 	private void RemoveLightList(int num)
 	{
 		m_lightList.RemoveAt(num);
-		m_lightPositionList.RemoveAt(num);
 	}
 
 	//  プレイヤー座標系設定
