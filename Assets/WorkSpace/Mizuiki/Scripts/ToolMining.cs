4273cb9c38d336dddfa0260f9584858881b959a9
batabatabutter <116615347+batabatabutter@users.noreply.github.com>
2024-06-24 10:29:29 +0900

    採掘強化UI追加

--- Assets/WorkSpace/Mizuiki/Scripts/ToolMining.cs
+++ Assets/WorkSpace/Mizuiki/Scripts/ToolMining.cs
@@ -6,8 +6,8 @@ using static BlockData;
 
 public class ToolMining : Tool
 {
-    [Header("ツールの種類"), CustomEnum(typeof(ToolData.ToolType))]
-    [SerializeField] private string toolTypeStr = "";
+	[Header("ツールの種類"), CustomEnum(typeof(ToolData.ToolType))]
+	[SerializeField] private string toolTypeStr = "";
 	private ToolData.ToolType m_toolType => SerializeUtil.Restore<ToolData.ToolType>(toolTypeStr);
 
 	[Header("強化する値(n倍)")]
@@ -17,12 +17,17 @@ public class ToolMining : Tool
 	[SerializeField] private float m_boostAmountValue = 2.0f;
 	// 効果が切れる値
 	private float m_amountValue = 0.0f;
+	//	効果がついた時の合計採掘ダメージ
+	private float m_startTakenDmg = 0.0f;
 
 	// プレイヤーのツールスクリプト
 	private PlayerTool m_playerTool = null;
 
 	// プレイヤーの採掘スクリプト
 	private PlayerMining m_playerMining = null;
+
+	//	ツール強化中
+	private bool m_isEnhance = false;
 
 
 	// Update is called once per frame
@@ -41,6 +46,9 @@ public class ToolMining : Tool
 			m_playerMining.MiningValueBoost -= m_boost;
 			// 採掘を削除する
 			m_playerMining = null;
+			//	フラグ設定
+			m_isEnhance = false;
+
 		}
 
 	}
@@ -60,10 +68,29 @@ public class ToolMining : Tool
 			// 強化終了値
 			m_amountValue = mining.TakenDamage + (mining.MiningPower * m_boostAmountValue);
 
+			//	初期値
+			m_startTakenDmg = mining.TakenDamage;
+
+            //	フラグ設定
+            m_isEnhance = true;
 		}
 
 		// ツールの取得
 		m_playerTool = obj.GetComponent<PlayerTool>();
 	}
 
+	//	終了値取得
+	public float AmountValue => m_amountValue;
+
+	//	初期値取得
+	public float StartTakenDMG => m_startTakenDmg;
+
+	//	フラグ取得
+	public bool IsEnhance => m_isEnhance;
+
+	//	タイプ取得
+	public ToolData.ToolType ToolType => m_toolType;
+
+	//	プレイヤー情報取得
+	public PlayerMining PlayerMining => m_playerMining;
 }
